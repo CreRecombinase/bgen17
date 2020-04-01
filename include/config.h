45012e8f06d7edb1887c0f37d9e8832c09edfc99
CreRecombinase <nwknoblauch@gmail.com>
2020-04-01 10:36:10 -0500

    first commit

---
+++ include/config.h
@@ -0,0 +1,7 @@
+#ifndef BGEN_REVISION_HPP
+#define BGEN_REVISION_HPP
+namespace globals {
+        char const* bgen_version = "1.1.6" ;
+        char const* const bgen_revision = "407eaf35542591417e617d611d3f0850a40a91fe" ;
+}
+#endif
